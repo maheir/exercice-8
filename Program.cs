8c06832f9df07956409e1ee59911073d32b665b7
maheir <maheirne@gmail.com>
2018-12-18 15:07:01 +0100

    EXERCICE 8 VARIABLE FIN

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PI = Math.PI, perimeter, area;
+            Console.WriteLine("rayon r d'un cercle");
+            int r = int.Parse(Console.ReadLine());
+            perimeter = 2 * PI * r;
+            Console.WriteLine("le périmétre est "+perimeter);
+            area = PI * (r*r);
+            Console.WriteLine("La surface est "+area);
+
+
+
+        }
+    }
+}
